21b051a2b89c64d1f7232c94b336dfe8f4fb7b1b
Paweł Wójcik <pawel.wojcik7@pollub.edu.pl>
2023-01-30 20:54:25 +0100

    Init

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      <h4>Kalkulator ciągu fibonnaciego</h4>
+        <p>Paweł Wójcik</p>
+        <p>I2S 2.5</p>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
